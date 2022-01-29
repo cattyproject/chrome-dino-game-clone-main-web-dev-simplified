ed3d7b91039b8bc63a95a4b6e954d0f638cff4b6
cattyproject <ivanilearningproject@gmail.com>
2022-01-29 15:00:56 +0700

    menambahkan fungsi setPixelToWorldScale

---
+++ script.js
@@ -0,0 +1,16 @@
+const WORLD_WIDTH =100
+const WORLD_HEIGHT=30
+
+const worldElem=document.querySelector('[data-world]')
+
+setPixelToWorldScale()
+window.addEventListener("resize", setPixelToWorldScale)
+
+function setPixelToWorldScale(){
+    let worldToPixelScale
+    if(window.innerWidth/window.innerHeight<WORLD_WIDTH/WORLD_HEIGHT){
+        worldToPixelScale = window.innerWidth / WORLD_WIDTH
+    } else {
+        worldToPixelScale = window.innerHeight / WORLD_HEIGHT
+    }
+}
