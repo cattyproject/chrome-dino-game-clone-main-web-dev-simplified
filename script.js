813ea081880027e6e3c2c20624a4d1e1c9088d84
cattyproject <ivanilearningproject@gmail.com>
2022-01-31 09:50:36 +0700

    membuat score pada game

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 import { setupGround, updateGround} from './ground.js'
 const WORLD_WIDTH =100
 const WORLD_HEIGHT=30
+const SPEED_SCALE_INCREASE= 0.00001
 
 const worldElem=document.querySelector('[data-world]')
 
 setPixelToWorldScale()
 window.addEventListener("resize", setPixelToWorldScale)
+document.addEventListener("keydown", handleStart, {once: true})
 
 setupGround()
 
 let lastTime
+let speedScale
+let score
 function update(time){
     if(lastTime == null){
         lastTime=time
@@ -20,13 +24,25 @@ function update(time){
 
     const delta=time -lastTime
 
-    updateGround(delta,1)
+    updateGround(delta,speedScale)
+    updateSpeedScale(delta)
+    updateScore(delta)
     
     lastTime=time
    
     window.requestAnimationFrame(update)
 }
-window.requestAnimationFrame(update)
+function updateSpeedScale(delta){
+    speedScale += delta * SPEED_SCALE_INCREASE
+}
+
+function handleStart(){
+    lastTime = null
+    speedScale = 1
+    score=0
+    setupGround()
+    window.requestAnimationFrame(update)
+}
 
 function setPixelToWorldScale(){
     let worldToPixelScale
